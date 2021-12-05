27007fc67790f3e056b05e9a4fb02465ff9e105b
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2021-12-05 22:07:36 +0530

    Update database.py

--- bot/database/database.py
+++ bot/database/database.py
@@ -1,5 +1,8 @@
 import motor.motor_asyncio # pylint: disable=import-error
 from bot import DB_URI
+from imdb import IMDb
+
+imdb = IMDb()
 
 class Singleton(type):
     __instances__ = {}
@@ -503,3 +506,65 @@ class Database(metaclass=Singleton):
         return await self.fcol.count_documents({"group_id": group_id})
 
 
+    async def donlee_imdb(query, bulk=False, id=False):
+    if not id:
+        # https://t.me/GetTGLink/4183
+        pattern = re.compile(r"^(([a-zA-Z\s])*)?\s?([1-2]\d\d\d)?", re.IGNORECASE)
+        match = pattern.match(query)
+        year = None
+        if match:
+            title = match.group(1)
+            year = match.group(3)
+        else:
+            title = query
+        movieid = imdb.search_movie(title.lower(), results=10)
+        if not movieid:
+            return None
+        if year:
+            filtered=list(filter(lambda k: str(k.get('year')) == str(year), movieid))
+            if not filtered:
+                filtered = movieid
+        else:
+            filtered = movieid
+        movieid=list(filter(lambda k: k.get('kind') in ['movie', 'tv series'], filtered))
+        if not movieid:
+            movieid = filtered
+        if bulk:
+            return movieid
+        movieid = movieid[0].movieID
+    else:
+        movieid = int(query)
+    movie = imdb.get_movie(movieid)
+    title = movie.get('title')
+    votes = str(movie.get("votes"))
+    country = str(movie.get("country"))
+    lang = str(movie.get("lang"))
+    runtime = str(movie.get("runtime"))
+    genres = ", ".join(movie.get("genres")) if movie.get("genres") else None
+    rating = str(movie.get("rating"))
+    if movie.get("original air date"):
+        date = movie["original air date"]
+    elif movie.get("year"):
+        date = movie.get("year")
+    else:
+        date = "N/A"
+    poster = movie.get('full-size cover url')
+    plot = movie.get('plot')
+    if plot and len(plot) > 0:
+        plot = plot[0]
+    if plot and len(plot) > 800:
+        plot = plot[0:800] + "..."
+    return {
+        'title': title,
+        'year': date,
+        'genres': genres,
+        'poster': poster,
+        'plot': plot,
+        'country': country,
+        'votes': votes,
+        'lang': lang,
+        'runtime': runtime,
+        'rating': rating,
+        'url':f'https://www.imdb.com/title/tt{movieid}'
+
+    }
