67ee71bb1df1df93d1486db4b4cb0cb83eaa9cf3
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2021-12-10 20:41:03 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,9 +142,6 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
 
-    ia = IMDBCONTROL
-    imdb = await donlee_imdb
-
     text=f"📂 ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : {query}\n📍Requested :- {update.from_user.mention}\n⚡️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : [<a href='https://t.me/Autofiltergroup_bot'>𝓐𝓵𝓫𝓮𝓻𝓽 𝓔𝓲𝓷𝓼𝓽𝓮𝓲𝓷〽️©️</a>]\n🔰 Group : {update.chat.title}\n👮‍♂ ɴᴏᴛɪᴄᴇ : <code>ɪ𝙵 ʏᴏᴜ ᴅᴏ ɴᴏᴛ sᴇᴇ ᴛʜᴇ 𝙵ɪʟᴇ𝚂 ᴏ𝙵 ᴛʜɪ𝚂 ᴍᴏᴠɪᴇ ʏᴏᴜ ᴀ𝚂ᴋᴇᴅ 𝙵ᴏʀ . ʟᴏᴏᴋ ᴀᴛ ɴᴇ𝚇ᴛ ᴘᴀɢᴇ</code>"
         
     try:
