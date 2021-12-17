425d1c794b399d0708a54fb09a35d0e5180f15da
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2021-12-17 18:21:42 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -20,11 +20,11 @@ async def start(bot, update):
     
     if file_uid:
         try:
-            member = await bot.get_chat_member(-1001569614628, update.chat.id)
+            member = await bot.get_chat_member(-1001372299086, update.chat.id)
             if member.status == "kicked":
                 await bot.send_message(
                        chat_id=update.chat.id,
-                       text="Sorry Brooh🤣!  You're  B A N N E D 🥱 Contact Admin @Myfreak123",
+                       text="Sorry mate!  You're  B A N N E D 🥱",
                        reply_to_message_id=update.message_id
                        )
                 return
@@ -33,9 +33,9 @@ async def start(bot, update):
             me = await bot.get_me()
             await bot.send_message(
                     chat_id=update.chat.id,
-                    text="You Need To Join Our Channel and Press Refresh Button to get the file.\n ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം Refresh ബട്ടൺ ക്ലിക്ക് ചെയ്ത് സ്റ്റാർട്ട്‌ കൊടുക്കുക ഫയൽ ലഭിക്കുന്നതാണ്✌️💗",
-                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Ⓜ️Join Channel ©️",url="https://t.me/+IK2aZWUBgjkwMjFl")],
-                                                       [InlineKeyboardButton(text="Ⓜ️ Refresh ©️", url=f"https://t.me/{me.username}?start={file_uid}")]]),
+                    text="You Need To Join Our Channel and Press Refresh Button to get the file.",
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Join Channel",url="https://t.me/joinchat/+IK2aZWUBgjkwMjFl")],
+                                                       [InlineKeyboardButton(text="Refresh", url=f"https://t.me/{me.username}?start={file_uid}")]]),
                     reply_to_message_id=update.message_id
                     )
             return
@@ -44,7 +44,7 @@ async def start(bot, update):
             print('Unable to Verify')
             await bot.send_message(
                      chat_id=update.chat.id,
-                     text="```Something Went Wrong Contact Admin @Myfreak123```",
+                     text="```Something Went Wrong```",
                      parse_mode='markdown',
                      reply_to_message_id=update.message_id
                      )
@@ -55,28 +55,64 @@ async def start(bot, update):
             return
         
         caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
-        try:
-            await update.reply_cached_media(
-                file_id,
-                quote=True,
-                caption =f"❤️ ꜰɪʟᴇ ɴᴀᴍᴇ:<code><b> {file_name}</b> </code>\n❤️ 𝚃𝚑𝚊𝚗𝚔𝚢𝚘𝚞 𝙵𝚘𝚛 𝚄𝚜𝚒𝚗𝚐 𝙾𝚞𝚛 𝚂𝚎𝚛𝚟𝚒𝚌𝚎 𝙿𝚕𝚎𝚊𝚜𝚎 𝚂𝚞𝚙𝚙𝚘𝚛𝚝 𝚄𝚜 𝙱𝚢 𝚂𝚑𝚊𝚛𝚒𝚗𝚐 𝙾𝚞𝚛 𝙲𝚑𝚊𝚗𝚗𝚎𝚕/𝙶𝚛𝚘𝚞𝚙 𝙻𝚒𝚗𝚔 𝚃𝚘 𝚈𝚘𝚞𝚛 𝙵𝚛𝚒𝚎𝚗𝚍𝚜 \n❁𝕁𝕠𝕚𝕟 𝕆𝕦𝕣 ℂ𝕙𝕒𝕟𝕟𝕖𝕝𝕤❁  \n⟱⟱⟱⟱⟱⟱⟱⟱⟱⟱⟱⟱⟱⟱  \n📌𝕮𝖍𝖆𝖓𝖓𝖊𝖑: @mcnewmovies➻ \n📌𝕮𝖍𝖆𝖓𝖓𝖊𝖑 : @mcmarvels➻ \n📌𝕮𝖍𝖆𝖓𝖓𝖊𝖑 : @Mclinkzz ➻",
+        
+        if file_type == "document":
+        
+            await bot.send_document(
+                chat_id=update.chat.id,
+                document = file_id,
+                caption = caption,
+                parse_mode="html",
+                reply_to_message_id=update.message_id,
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton
+                                (
+                                    'Developers', url="https://t.me/CrazyBotsz"
+                                )
+                        ]
+                    ]
+                )
+            )
+
+        elif file_type == "video":
+        
+            await bot.send_video(
+                chat_id=update.chat.id,
+                video = file_id,
+                caption = caption,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton('𝚂𝙷𝙰𝚁𝙴', url="https://t.me/share/url?url=https://t.me/share/url?url=https://t.me/Movies_Club_2019")
-                ],
-                [
-                    InlineKeyboardButton('𝙼𝙾𝚅𝙸𝙴 𝚁𝙴𝚀', url="https://t.me/Movies_Club_2019"),
-                    InlineKeyboardButton('𝚂𝙴𝚁𝙸𝙴𝚂', url="https://t.me/MoviesClubSeriesonly")
-                ],
-                [
-                    InlineKeyboardButton('𝙼𝙲 𝙻𝙸𝙽𝙺𝚉', url="https://t.me/Mclinkzz"),
-                    InlineKeyboardButton('𝚄𝙿𝙳𝙰𝚃𝙴𝚂', url="https://t.me/+IK2aZWUBgjkwMjFl")
-                ]
-            ]
-        )
-    )
+                    [
+                        [
+                            InlineKeyboardButton
+                                (
+                                    'Developers', url="https://t.me/Rihu_mone"
+                                )
+                        ]
+                    ]
+                )
+            )
+            
+        elif file_type == "audio":
+        
+            await bot.send_audio(
+                chat_id=update.chat.id,
+                audio = file_id,
+                caption = caption,
+                parse_mode="html",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton
+                                (
+                                    'Developers', url="https://t.me/Rihu_mone"
+                                )
+                        ]
+                    ]
+                )
+            )
 
         else:
             print(file_type)
@@ -84,8 +120,12 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('🤴 𝙳𝙴𝚅 🤴', url="https://t.me/BlinderTG"),
-        InlineKeyboardButton('🚫 𝙲𝚕𝚘𝚜𝚎 🚫', callback_data='close')
+        InlineKeyboardButton('Developers', url='https://t.me/Rihu_mone'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://t.me/BWF_ofc/2')
+    ],[
+        InlineKeyboardButton('Support 🛠', url='https://t.me/joinchat/c-r6fPf_UqwxMmE9')
+    ],[
+        InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
