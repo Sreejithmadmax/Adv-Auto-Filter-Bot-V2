b0ccf49d5575c5e21b189ba7b0e4fcfc8313c775
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2021-12-19 16:05:38 +0530

    Add files via upload

---
+++ bot/Simple_Config.py
@@ -0,0 +1,7 @@
+import os
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+class Mo_Tech_YT(object):
+    
+    MO_TECH_YT_05 = os.environ.get("EXPELL").split()  
+    MO_TECH_YT_08 = os.environ.get("IN_EXPELL").split()   
+    
