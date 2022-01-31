5660377d933c997c75f5d30c1796081ce473cadf
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-01-31 21:56:29 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -109,7 +109,7 @@ async def auto_filter(bot, update):
     if filters:
         results.append(
                 [
-                    InlineKeyboardButton(f"🔮{query}🔮", callback_data="my_instructs")
+                    InlineKeyboardButton(f"🔮{query}🔮", callback_data="instructions")
                 ]
             ) 
 
