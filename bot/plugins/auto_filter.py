bb1233ee29a80fa8576b25848b5457aed8ede15a
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-02-01 14:28:21 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -57,7 +57,7 @@ async def auto_filter(bot, update):
 
             chat_id=update.chat.id,
 
-            text="<b><u><i>⭕️സിനിമയുടെ പേര് മാത്രം അയക്കൂ സുഹൃത്തേ🙏\n\n⭕️Please Sent Movie Name Only🤧\n\n⭕️Search Full Result Of ᴰᵁᴰᴱ ᴬᴰᴴᴼᴸᴷᴴᴬᴹ\n\nOr Contact Admin 👉@BlinderTG....</u></i></b>",
+            text="<b><u><i>⭕️സിനിമയുടെ പേര് മാത്രം അയക്കൂ സുഹൃത്തേ🙏\n\n⭕️Please Sent Movie Name Only🤧\n\n⭕️Search Full Result Of bots🤖\n\nOr Contact Admin 👉@UrvashiTheatersAdmin_Bot....</u></i></b>",
 
             reply_to_message_id=update.message_id
 
